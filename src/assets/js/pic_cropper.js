5b6e57b6affbbf5e0deff5e183bab26793aefaa6
samuelitwaru <samuelitwaru@gmail.com>
2019-08-07 09:29:59 +0300

    Image cropper

--- src/assets/js/pic_cropper.js
+++ src/assets/js/pic_cropper.js
@@ -1,53 +1,50 @@
 var avatar;
 var image;
-var inputs;
+var input;
 var $modal;
 var cropper;
 
 window.addEventListener('DOMContentLoaded', function(){
 	image = document.getElementById('image');
-	inputs = $('.input');
+	input = $('#logo');
 	$modal = $('#modal');
+	avatar = document.getElementById('_logo');
 
-	for(var i=0; i < inputs.length; i++) {
-		input = inputs[i]
-		input.addEventListener('change', function (e) {
-			var files = e.target.files;
-			var done = function (url) {
-				input.value = '';
-				image.src = url;
-				$modal.modal('show');
+	input.on('change', function (e) {
+		var files = e.target.files;
+		var done = function (url) {
+			input.value = '';
+			image.src = url;
+			$modal.modal('show');
+		};
+
+		var reader;
+		var file;
+		var url;
+
+		if (files && files.length > 0) {
+			file = files[0];
+
+			if (URL) {
+				done(URL.createObjectURL(file));
+			} else if (FileReader) {
+				reader = new FileReader();
+				reader.onload = function (e) {
+				done(reader.result);
 			};
-			var reader;
-			var file;
-			var url;
-
-			avatar = document.getElementById('_'+event.target.id);
-			console.log(avatar)
-
-			if (files && files.length > 0) {
-				file = files[0];
-
-				if (URL) {
-					done(URL.createObjectURL(file));
-				} else if (FileReader) {
-					reader = new FileReader();
-					reader.onload = function (e) {
-					done(reader.result);
-				};
-				
-				reader.readAsDataURL(file);
-				
-				}
+			
+			reader.readAsDataURL(file);
+			
 			}
+		}
 
-		});	
-	}
+	});	
+	
 
 	$modal.on('shown.bs.modal', function () {
 		cropper = new Cropper(image, {
 			  aspectRatio: 1,
-			  viewMode: 3,
+			  viewMode: 1,
 		});
 	}).on('hidden.bs.modal', function () {
 		cropper.destroy();
@@ -65,18 +62,12 @@ window.addEventListener('DOMContentLoaded', function(){
 			width: 160,
 			height: 160,
 		  });
-		  console.log()
+
 		  initialAvatarURL = avatar.src;
 		  avatar.src = canvas.toDataURL();
 		}
 	});
 
-	var images = $('#_img1, #_img2');
-	for (var i=0; i<images.length; i++){
-		var img = images[i];
-		console.log(img)
-		var input = $('#'+img.id.replace("_", ""));
-		console.log(input)
-		//img.onclick = function(){input.trigger('click')}
-	}
+	avatar.onclick = function(){input.trigger('click')}
+
 })
